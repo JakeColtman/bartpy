eb19ce73cc2f37a6f8a7ba3fc717b57c5548153d
Jake Coltman <jakecoltman@sky.com>
2018-07-26 07:48:48 +0100

    Starting out

---
+++ bartpy/model.py
@@ -0,0 +1,6 @@
+
+
+class Model:
+
+    def __init__(self, ):
+        pass
---
+++ bartpy/tree.py
@@ -0,0 +1,10 @@
+
+
+class TreeStructure:
+
+    def __init__(self):
+        pass
+
+    def predict(self, X):
+        pass
+
