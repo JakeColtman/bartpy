9e78da337bbcab6f3d2d98ec6495709de74867f6
Jake Coltman <jakecoltman@sky.com>
2018-09-15 13:52:22 +0100

    Allow access to custom sampling schedules

--- examples/sin.py
+++ examples/sin.py
@@ -6,11 +6,11 @@ from bartpy.plotting import plot_residuals, plot_modelled_against_actual
 
 
 def run(alpha, beta, n_trees):
-    x = np.sin(np.linspace(0, 5, 12000)).reshape((3000, 4))
+    x = np.sin(np.linspace(0, 5, 3000))
     X = pd.DataFrame(x)
-    y = np.random.normal(0, 0.1, size=3000) + x.mean(axis=1)
+    y = np.random.normal(0, 0.1, size=3000) + x
 
-    model = SklearnModel(n_samples=200, n_burn=50, n_trees=n_trees, alpha=alpha, beta=beta)
+    model = SklearnModel(n_samples=50, n_burn=50, n_trees=n_trees, alpha=alpha, beta=beta)
     model.fit(X, y)
     predictions = model.predict()
     plot_residuals(model)
