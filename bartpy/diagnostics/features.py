743378f7f5001b1b3501430838fc8575d6d8a4b0
Jake Coltman <jakecoltman@sky.com>
2018-10-27 07:49:15 +0100

    Add a method to calculate null distribution of feature inclusion in splits

--- bartpy/diagnostics/features.py
+++ bartpy/diagnostics/features.py
@@ -1,10 +1,11 @@
 from collections import Counter
-from typing import List, Tuple
+from typing import Mapping, List, Tuple
 
 from matplotlib import pyplot as plt
 import numpy as np
 
 from bartpy.model import Model
+from bartpy.sklearnmodel import SklearnModel
 
 
 def feature_split_proportions_counter(model_samples: List[Model]) -> Tuple[int, Counter]:
@@ -29,3 +30,41 @@ def plot_feature_split_proportions(model_samples: List[Model]):
     plt.yticks(y_pos, name)
     plt.xlabel('Proportion of all splits')
     plt.show()
+
+
+def null_feature_split_proportions_distribution(model: SklearnModel,
+                                                X: np.ndarray,
+                                                y: np.ndarray,
+                                                n_permutations: int=10) -> Mapping[int, List[float]]:
+    """
+    Calculate a null distribution of proportion of splits on each variable in X
+
+    Works by randomly permuting y to remove any true dependence of y on X and calculating feature importance
+
+    Parameters
+    ----------
+    model: SklearnModel
+        Model specification to work with
+    X: np.ndarray
+        Covariate matrix
+    y: np.ndarray
+        Target data
+    n_permutations: int
+        How many permutations to run
+        The higher the number of permutations, the more accurate the null distribution, but the longer it will take to run
+    Returns
+    -------
+    Mapping[int, List[float]]
+        A list of inclusion proportions for each variable in X
+    """
+
+    inclusion_dict = {x: [] for x in range(X.shape[1])}
+
+    for _ in range(n_permutations):
+        y_perm = np.random.permutation(y)
+        model.fit(X, y_perm)
+        splits_perm = feature_split_proportions_counter(model)
+        for key, value in splits_perm:
+            inclusion_dict[key].append(value)
+
+    return inclusion_dict
