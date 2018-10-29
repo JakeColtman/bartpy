0826a0d5a731c5d3719f7d3c4a9d642fd9180e27
Jake Coltman <jakecoltman@sky.com>
2018-10-29 08:28:38 +0000

    Add a diagnostics plot of homoskedasity

--- bartpy/diagnostics/residuals.py
+++ bartpy/diagnostics/residuals.py
@@ -1,5 +1,6 @@
-import statsmodels.api as sm
 from matplotlib import pyplot as plt
+import seaborn as sns
+import statsmodels.api as sm
 
 from bartpy.sklearnmodel import SklearnModel
 
@@ -10,3 +11,13 @@ def plot_qq(model: SklearnModel, ax=None) -> None:
     residuals = model.residuals(model.data.X)
     sm.qqplot(residuals, fit=True, line="45", ax=ax)
     ax.set_title("QQ plot")
+
+
+def plot_homoskedasity_diagnostics(model: SklearnModel, ax=None):
+    if ax is None:
+        fig, ax = plt.subplots(1, 1, figsize=(5, 5))
+    sns.regplot(model.predict(model.data.X), model.residuals(model.data.X))
+    ax.set_title("Fitted Values V Residuals")
+    ax.set_xlabel("Fitted Value")
+    ax.set_ylabel("Residual")
+    return ax
