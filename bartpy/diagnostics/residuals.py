841b8168d8a4e1efd85ba3ef5c4ed015dc2bd830
Jake Coltman <jakecoltman@sky.com>
2018-10-31 21:31:02 +0000

    Plot diagnostics onto correct ax object

--- bartpy/diagnostics/residuals.py
+++ bartpy/diagnostics/residuals.py
@@ -13,10 +13,11 @@ def plot_qq(model: SklearnModel, ax=None) -> None:
     ax.set_title("QQ plot")
     return ax
 
+
 def plot_homoskedasity_diagnostics(model: SklearnModel, ax=None):
     if ax is None:
         fig, ax = plt.subplots(1, 1, figsize=(5, 5))
-    sns.regplot(model.predict(model.data.X), model.residuals(model.data.X))
+    sns.regplot(model.predict(model.data.X), model.residuals(model.data.X), ax=ax)
     ax.set_title("Fitted Values V Residuals")
     ax.set_xlabel("Fitted Value")
     ax.set_ylabel("Residual")
